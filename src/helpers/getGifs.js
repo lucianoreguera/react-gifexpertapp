08e6d72d788e770fb3ca9dfbc58954ccdf875742
Luciano <regueraluciano@gmail.com>
2020-10-21 12:08:32 -0300

    error url fixed

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 // encodeURI() limpia la url, espacios en blanco etc
 export const getGifs = async ( category ) => {
-    const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=UUubMjczgjVvOWs51iSQ0AwuwxKuwt2c`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=UUubMjczgjVvOWs51iSQ0AwuwxKuwt2c`;
     const response = await fetch(url);
     const { data } = await response.json();
 
