d0415dff47607040d1eb4867fd827cbe1eb30334
Luciano <regueraluciano@gmail.com>
2020-10-21 11:46:51 -0300

    Add Docs

---
+++ docs/precache-manifest.8572dd9ba046da9004501f782a640d6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "248d4d053b1f27e0773378ec4c22de2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e10b78a1481d8e8fea6",
+    "url": "/static/css/main.5d0f7706.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e10b78a1481d8e8fea6",
+    "url": "/static/js/main.48459273.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
